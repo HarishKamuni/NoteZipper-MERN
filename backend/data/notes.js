13384a42a86eb1a69aa5c6019cd2a4d04a903148
Harish Kamuni <108679987+HarishKamuni@users.noreply.github.com>
2023-09-14 14:09:37 +0530

    Initializzed our backend and frontend

---
+++ backend/data/notes.js
@@ -0,0 +1,29 @@
+const notes = [
+  {
+    _id: '1',
+    title: 'first',
+    content: 'first data',
+  },
+  {
+    _id: '2',
+    title: 'second',
+    content: 'second data',
+  },
+  {
+    _id: '3',
+    title: 'third',
+    content: 'third data',
+  },
+  {
+    _id: '4',
+    title: 'four',
+    content: 'four data',
+  },
+  {
+    _id: '5',
+    title: 'five',
+    content: 'five data',
+  },
+];
+
+module.exports = notes;
